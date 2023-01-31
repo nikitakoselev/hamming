4180ad4cf0b02afe2ac05202e4bf4489125b2487
jkosha <nikita.koselev@gmail.com>
2023-01-31 19:55:13 +0000

    tests are passing

--- hamming.go
+++ hamming.go
@@ -1,5 +1,19 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	var result = 0
+
+	if len(a) != len(b) {
+		return 0, errors.New("strings' length is different")
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			result += 1
+		}
+	}
+
+	return result, nil
 }
